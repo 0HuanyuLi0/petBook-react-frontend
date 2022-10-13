1a04c3d44b5e0e031161eed4064aab7bd688963c
Huanyu Li <lihuanyu1024@outlook.com>
2022-10-14 10:24:45 +1030

    before seeds v3-change to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ import React from 'react'
 //   Route,
 //   Routes,
 //   Link,
+//   HashRouter,
 // } from "react-router-dom";
 
-import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Link, Route, Routes} from 'react-router-dom';
 
 import Home from './components/home/Home';
 import Login from './components/login/Login';
