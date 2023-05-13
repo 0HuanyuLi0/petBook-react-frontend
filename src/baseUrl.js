db0a2ff59ad5cfd7edf50900d3a196b690da2337
Huanyu Li <lihuanyu1024@outlook.com>
2023-05-13 21:17:05 +1000

    change hoster to render

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -4,7 +4,7 @@ let BASE_URL;
 if( process.env.NODE_ENV === 'development'){
     BASE_URL = 'http://localhost:3000/';
 } else {
-    BASE_URL = 'https://54.213.130.218:3000/';
+    BASE_URL = 'https://petbook-nodejs-backend.onrender.com/';
     // BASE_URL = 'https://petbook-server-huanyuli.herokuapp.com/';
 }
 
